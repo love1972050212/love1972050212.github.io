33a1552ddca7a724ba754cbc7d725d0414e6ed2e
kirito <114930301+love1972050212@users.noreply.github.com>
2024-02-25 23:00:02 +0800

    上傳

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,16 +3,16 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="w-full h-screen">
-    <div class="w-full h-full flex flex-col">
-        <div class="w-full">
+  <div class="w-full h-screen bg-indigo-500">
+    <div class="w-1/2 m-auto h-full flex flex-col bg-white gap-4">
+        <div class="w-full flex ml-1">
            <div class="w-2/4"><input class="input" type="text" id="" placeholder="請注意事項"></div>
-            <button class="right">新增</button>
+            <button class="right bg-indigo-500">新增</button>
         </div>
-        <div class="w-full flex">
-            <button class="" data-search="all">全部</button>
-            <button class="" data-search="isTodo">已執行</button>
-            <button class="" data-search="notTodo">未執行</button>
+        <div class="w-full flex gap-3 ml-1">
+            <button class="bg-indigo-500 ..." data-search="all">全部</button>
+            <button class="bg-indigo-500 ..." data-search="isTodo">已執行</button>
+            <button class="bg-indigo-500 ..." data-search="notTodo">未執行</button>
         </div>
         <div class="w-full flex">
                 <div class="flex-1">執行</div>
